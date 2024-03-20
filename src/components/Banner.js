f769ea1f468799a8fdae88b291c3fa64efbe2cf1
elisa <caillaud.elisa86@gmail.com>
2024-03-20 15:07:14 +0100

    preparation

---
+++ src/components/Banner.js
@@ -0,0 +1,18 @@
+import data from '../datas/logements.json';
+
+function Logements(){
+  return (
+    <ul>
+        {data.map((logement) => (
+          <ul>
+            <li>{ logement.title }</li>
+            <li>{ logement.description }</li>
+            <li>Hote: { logement.host.name }</li>
+            <li><img src={logement.cover}/></li>
+          </ul>
+        ))}
+    </ul>
+)
+}
+
+export default Logements;
