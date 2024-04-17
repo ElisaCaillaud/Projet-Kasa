9a82cc1e4b5bb7bfef290cf29bb581c3907bfb1a
elisa <caillaud.elisa86@gmail.com>
2024-04-17 17:21:40 +0200

    ajout css

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,34 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "../../styles/header.scss";
 import logo from "../../assets/logo.png";
 
 function Header() {
+  const location = useLocation();
+
   return (
     <div className="nav-full">
       <a href="/">
         <img src={logo} alt="logo Kasa" className="img-logo" />
       </a>
       <nav className="nav-menu">
-        <Link to="/">Accueil</Link>
-        <Link to="/a-propos">A propos</Link>
+        <Link
+          to="/"
+          className={
+            location.pathname === "/" ? "nav-link underline" : "nav-link"
+          }
+        >
+          Accueil
+        </Link>
+        <Link
+          to="/a-propos"
+          className={
+            location.pathname === "/a-propos"
+              ? "nav-link underline"
+              : "nav-link"
+          }
+        >
+          A Propos
+        </Link>
       </nav>
     </div>
   );
